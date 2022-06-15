e09e3fec18fc7e8fd73753e3e0352f05772cf15b
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-06-15 15:00:09 -0500

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_to_98 -  function that prints all natural numbers from n to 98,
+*	followed by a new line.
+*
+* @n: input to do print the numbers until.
+*/
+
+void print_to_98(int n)
+{
+	int i = 0;
+
+	if (n > 98)
+	{
+		for (i = n ; i > 98 ; i--)
+			printf("%d, ", i);
+	} 
+    else
+	{
+		for (i = n ; i < 98 ; i++)
+			printf("%d, ", i);
+	}
+	printf("98");
+	printf("\n");
+}
