9a309b3983a5cbe053f9178001532bdd0af7bd5a
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-07-06 12:44:15 -0500

    A library is not a luxury but one of the necessities of life

---
+++ static_libraries/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
