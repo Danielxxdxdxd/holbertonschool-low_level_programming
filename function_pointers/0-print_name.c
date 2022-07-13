a3ed6da03b1673bac89c4baae829d984ce3862d5
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-07-13 13:52:56 -0500

    What's my name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name.
+ * @name: char to print
+ * @f: pointer to function
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
