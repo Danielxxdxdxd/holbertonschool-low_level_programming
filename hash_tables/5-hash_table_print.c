31f6f7974e2f264bba4f2fc59f5786e713f63084
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-08-30 19:55:22 -0500

    >>> print(ht)

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+#define TRUE 1
+#define FALSE 0
+
+/**
+ * hash_table_print - print elements of the hash table
+ *@ht: hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int comma = 1;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i <= ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			if (!comma)
+				printf(", ");
+			else
+				comma = 0;
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+		}
+	}
+	printf("}\n");
+	printf("%lu", ht->size);
+}
