709fd52b7d6051265ec81ce3cc387ce72d5d4567
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-08-30 19:55:38 -0500

    >>> ht['betty']

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get a value for a key
+ * @ht: hash table input
+ * @key: key for the hash table
+ * Return: NULL if failure
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[index])
+	{
+		if (strcmp(ht->array[index]->key, (const char *)key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (NULL);
+}
