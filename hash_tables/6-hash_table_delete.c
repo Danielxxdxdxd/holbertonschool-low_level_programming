e1efa906d6db9056e9761340260bf4d3ff510d95
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-08-30 19:55:12 -0500

    >>> del ht

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table input
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp = NULL;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			while (ht->array[i])
+			{
+				tmp = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = tmp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
